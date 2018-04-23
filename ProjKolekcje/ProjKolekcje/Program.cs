87ad58c0b27de0f52a035752443aeeb77833b7e4
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-04-23 18:18:35 +0200

    a

---
+++ ProjKolekcje/ProjKolekcje/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjKolekcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList();
+            arr.Add("A");
+            arr.Add("B");
+            arr.Add("C");
+            arr.Add("D");
+            arr.Add("E");
+            
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+
+            for (int i = arr.Count; i > 0; i--)
+            {
+                int index = arr.Count - i;
+                dic.Add(index, arr[i-1].ToString());
+            }
+
+            dic.Remove(3);           
+        }
+    }
+}
